f5df09c37c98ea2d57f53a5d2c1d618f32ef23e8
Nox917 <mohamedalmustafa917@gmail.com>
2025-10-21 20:36:18 +0200

    adding the css and js files

---
+++ index.js
@@ -0,0 +1,28 @@
+const seedColor = document.getElementById("seed-color")
+const options = document.getElementById("scheme-options")
+const schemeColors = document.getElementById("scheme-colors")
+
+document.getElementById("scheme-form").addEventListener("submit" , (e)=>{
+    e.preventDefault()
+    console.log(seedColor.value , options.value)
+    
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor.value.slice(1)}&mode=${options.value}&count=5`)
+    .then(res => res.json())
+    .then(data => {
+        let html = ''
+        let hexes = ''
+        for( let color of data.colors){
+            html += `
+            <div class="scheme-color" style="background-color:${color.hex.value} ;"></div>
+            `
+            hexes += `
+                <p class = "hexes">${color.hex.value}</p>
+            `
+        }
+
+    document.getElementById("scheme-colors").innerHTML = html
+    document.querySelector(".hex-names").innerHTML = hexes
+        
+    })
+})
+
